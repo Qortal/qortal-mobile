71ad2cc81dc1be326e2f312add84f4a4294acd37
PhilReact <philliplangmartinez@gmail.com>
2024-11-25 09:57:31 +0200

    update version

--- src/ExtStates/NotAuthenticated.tsx
+++ src/ExtStates/NotAuthenticated.tsx
@@ -23,7 +23,7 @@ import { set } from "lodash";
 import { cleanUrl, isUsingLocal } from "../background";
 
 export const manifestData = {
-  version: '0.3.3'
+  version: '0.3.4'
 }
 
 export const NotAuthenticated = ({
