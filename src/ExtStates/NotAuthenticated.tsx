e9bb8b465c9e36adf5194363c7d36da8d079476a
PhilReact <philliplangmartinez@gmail.com>
2025-07-04 21:22:59 +0300

    release 0.5.4

--- src/ExtStates/NotAuthenticated.tsx
+++ src/ExtStates/NotAuthenticated.tsx
@@ -28,7 +28,7 @@ import { GlobalContext } from "../App";
 import Tooltip, { TooltipProps, tooltipClasses } from '@mui/material/Tooltip';
 
 export const manifestData = {
-  version: "0.5.3",
+  version: "0.5.4",
 };
 
 
