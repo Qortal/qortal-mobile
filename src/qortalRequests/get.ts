d0bde278bb805d648e627b2ffddb3b8cbb6c980f
PhilReact <philliplangmartinez@gmail.com>
2025-09-07 21:47:51 +0300

    added isMultiFileZip

--- src/qortalRequests/get.ts
+++ src/qortalRequests/get.ts
@@ -955,7 +955,7 @@ export const publishQDNResource = async (
   const file = data?.file || data?.blob;
   const tags = data?.tags || [];
   const result = {};
-
+  const isMultiFileZip = data?.isMultiFileZip === true;
 
   if (file && file.size > MAX_SIZE_PUBLISH) {
     throw new Error(
@@ -1051,7 +1051,7 @@ export const publishQDNResource = async (
         data: data64 ? data64 : file,
         service: service,
         identifier: encodeURIComponent(identifier),
-        uploadType: data64 ? 'base64' : 'file',
+        uploadType: isMultiFileZip ? 'zip' : data64 ? 'base64' : 'file',
         filename: filename,
         title,
         description,
@@ -1346,6 +1346,8 @@ export const publishMultipleQDNResources = async (
       const description = resource.description;
       const category = resource.category;
       const tags = resource?.tags || [];
+      const isMultiFileZip = resource?.isMultiFileZip === true;
+
       const result = {};
       // Fill tags dynamically while maintaining backward compatibility
       for (let i = 0; i < 5; i++) {
@@ -1405,7 +1407,9 @@ export const publishMultipleQDNResources = async (
 
       try {
         const dataType =
-          resource?.base64 || resource?.data64 || resourceEncrypt
+         isMultiFileZip
+          ? 'zip'
+          : resource?.base64 || resource?.data64 || resourceEncrypt
             ? 'base64'
             : 'file';
        const response = await publishData({
